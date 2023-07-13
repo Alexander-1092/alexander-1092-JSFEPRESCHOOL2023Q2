c08b3f039695fb2b467705fe2ca794437ec5157c
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2023-07-13 20:42:08 +0300

    feat: chackbox in js

---
+++ library/index.js
@@ -0,0 +1,64 @@
+const checkboxWinter = document.getElementById('input-winter')
+const checkboxSpring = document.getElementById('input-spring')
+const checkboxSummer = document.getElementById('input-summer')
+const checkboxAutumn = document.getElementById('input-autumn')
+
+const labelWinter = document.getElementById('label-winter')
+const labelspring = document.getElementById('label-spring')
+const labelSummer = document.getElementById('label-summer')
+const labelAutumn = document.getElementById('label-autumn')
+
+
+function checkCheckboxWinter(){
+	if (checkboxWinter.checked) {
+		labelspring.classList.remove('open')
+		labelSummer.classList.remove('open')
+		labelAutumn.classList.remove('open')
+		labelWinter.classList.add('open')
+	}
+}
+
+checkboxWinter.addEventListener('change', function(){
+	checkCheckboxWinter()
+})
+
+function checkCheckboxSpring(){
+	if (checkboxSpring.checked) {
+		labelSummer.classList.remove('open')
+		labelAutumn.classList.remove('open')
+		labelWinter.classList.remove('open')
+		labelspring.classList.add('open')
+	}
+}
+
+checkboxSpring.addEventListener('change', function(){
+	checkCheckboxSpring()
+})
+
+function checkCheckboxSummer(){
+	if (checkboxSummer.checked) {
+		labelAutumn.classList.remove('open')
+		labelWinter.classList.remove('open')
+		labelspring.classList.remove('open')
+		labelSummer.classList.add('open')
+	}
+}
+
+checkboxSummer.addEventListener('change', function(){
+	checkCheckboxSummer()
+})
+
+function checkCheckboxAutumn(){
+	if (checkboxAutumn.checked) {
+		labelWinter.classList.remove('open')
+		labelspring.classList.remove('open')
+		labelSummer.classList.remove('open')
+		labelAutumn.classList.add('open')
+	}
+}
+
+checkboxAutumn.addEventListener('change', function(){
+	checkCheckboxAutumn()
+})
+
+checkCheckboxWinter()
