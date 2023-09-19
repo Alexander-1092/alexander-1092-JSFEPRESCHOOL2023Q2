c8234bb48e0d83bbb31069c6e0f6823bd73f60af
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2023-09-19 20:36:04 +0300

    fix: bag btn pause

--- audio-player/index.js
+++ audio-player/index.js
@@ -2,40 +2,87 @@ const audio = document.querySelector('audio');
 const playBtn = document.querySelector('.play-btn');
 const picPlay = document.querySelector('.pic-play')
 const picStop = document.querySelector('.pic-stop')
+const picNext = document.querySelector('.pic-btn-next')
+const picPrev = document.querySelector('.pic-btn-prev')
+const boxBtn = document.querySelector('.box-btn')
+
+
+let playNum = 0
+
+const listSongs = [
+	'./assets/media/Nirvana_-_Smells_Like_Teen_Spirit_47829249.mp3',
+	'./assets/media/Elvis Presley — JAILHOUSE ROCK.mp3',
+	'./assets/media/Goombay-Dance-Band_-_Seven-Tears.mp3',
+	'./assets/media/Frank_Sinatra_-_Strangers_in_the_Night_(musmore.com).mp3',
+	'./assets/media/Little Richard - Tutti-Frutti.mp3'
+]
+
 
 ////btn plae and stop
 isPlay = false
 
 function playAudio() {
+
 	if (isPlay === false) {
+		console.log(audio.duration)
 		audio.currentTime = 0;
+		listenListSong()
 		ShowPicBtn()
   	audio.play();
 		isPlay = true
-	} else {
-		ShowPicBtn()
+	} else {	
 		pauseAudio()
+		ShowPicBtnStop()
 		isPlay = false
 	}
 }
+
 
 function pauseAudio() {
   audio.pause();
 }
 
+
 function ShowPicBtn() {
-	picPlay.classList.toggle('pic-player-inactive')
-	picStop.classList.toggle('pic-player-active')
+	picPlay.classList.add('pic-player-inactive')
+	picStop.classList.add('pic-player-active')
+}
+
+function ShowPicBtnStop() {
+	picPlay.classList.remove('pic-player-inactive')
+	picStop.classList.remove('pic-player-active')
 }
 
 
-playBtn.addEventListener('click', () => {
-	playAudio()
-});
-pauseBtn.addEventListener('click', () => {
-	pauseAudio()
-});
+function nextSong() {
+	listSongs.length - 1 === playNum ? playNum = 0 : playNum++
+}
+
+function prevSong() {
+	playNum === 0 ? playNum = listSongs.length - 1 : playNum--
+}
 
 
+picNext.addEventListener('click', () => {
+	nextSong(); 
+	if (isPlay === true) {isPlay = false; playAudio();
+	} else {isPlay = false; ShowPicBtn();	playAudio();
+	}
+})
 
-////
+picPrev.addEventListener('click', () => {
+	prevSong(); 
+	if (isPlay === true) {isPlay = false; playAudio();
+	} else {isPlay = false; playAudio();
+	}
+})
+
+
+playBtn.addEventListener('click', () => {playAudio()});
+
+pauseBtn.addEventListener('click', () => {pauseAudio()});
+
+function listenListSong() {
+	audio.setAttribute('src', listSongs[playNum])
+
+}
