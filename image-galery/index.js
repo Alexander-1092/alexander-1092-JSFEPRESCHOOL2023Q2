9ec6c45a303bd9bfe3b23aa80c8fa52fd214913f
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2023-09-26 21:57:15 +0300

    feat: add pic api

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,11 +1,31 @@
-const requestURL = 'https://api.unsplash.com/photos/random?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo'
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.search')
+const picBox = document.querySelector('.pic-box')
 
+let searchWord = 'winter'
 
-function getData() {
-  fetch(requestURL)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+async function GetPic() {
+	const url = `https://api.unsplash.com/search/photos?query=${searchWord}&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`
+	try {
+		const response = await fetch(url)
+		const data = await response.json()
+		for (let index = 0; index < 10; index++) {
+			let link = String(data.results[index].urls.raw)
+			document.querySelectorAll('.pic')[index].src = link	
+		}
+	} catch (error) {
+		console.log(error)
+	}
 }
-getData();
+
+btn.addEventListener('click', ()=> {
+	searchWord = input.value
+	GetPic()
+})
+
+
+for (let index = 0; index < 10; index++) {
+	let photo = document.createElement('a')
+	photo.innerHTML = `<img src="" alt="logo" class="pic">`
+	picBox.appendChild(photo)
+}
