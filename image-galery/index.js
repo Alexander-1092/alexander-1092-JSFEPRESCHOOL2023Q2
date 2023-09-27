7476518a9f85cb14e83ea02d829b84692b5bb0aa
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2023-09-27 19:00:07 +0300

    feat: add show full pic

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,6 +1,11 @@
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.search')
 const picBox = document.querySelector('.pic-box')
+const mainPic = document.querySelector('.main-pic')
+const showPic = document.querySelector('.show-pic')
+const container = document.querySelector('.wrapper')
+const picClose = document.querySelector('.pic-close')
+
 
 let searchWord = 'winter'
 
@@ -9,8 +14,9 @@ async function GetPic() {
 	try {
 		const response = await fetch(url)
 		const data = await response.json()
+		console.log(data)
 		for (let index = 0; index < 10; index++) {
-			let link = String(data.results[index].urls.raw)
+			let link = String(data.results[index].urls.regular)
 			document.querySelectorAll('.pic')[index].src = link	
 		}
 	} catch (error) {
@@ -29,3 +35,21 @@ for (let index = 0; index < 10; index++) {
 	photo.innerHTML = `<img src="" alt="logo" class="pic">`
 	picBox.appendChild(photo)
 }
+
+
+picBox.addEventListener('click', (event) => {
+	container.classList.add('wrapper-inactive')
+	showPic.classList.add('show-pic-active')
+	mainPic.classList.add('main-pic-active')
+	picClose.classList.add('pic-close-active')
+	mainPic.src = event.target.src
+})
+
+
+picClose.addEventListener('click', (e) => {
+	container.classList.remove('wrapper-inactive')
+	showPic.classList.remove('show-pic-active')
+	mainPic.classList.remove('main-pic-active')
+	picClose.classList.remove('pic-close-active')
+	mainPic.src = ''
+})
